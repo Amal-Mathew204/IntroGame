f16528459f72a8024a0e4d92f9bd62c821d4098c
Amal-Mathew204 <119940686+Amal-Mathew204@users.noreply.github.com>
2024-09-30 21:17:57 +0100

    Updated Player Controller Script
    The script has been updated to adjusting text fields during gameplay and updated package version of textmeshpro to use latest

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
-
     private int count;
+    private int numPickups = 5;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
     void Start()
     {
         count = 0;
+        winText.text = "";
+        SetCountText();
         
     }
     void OnMove(InputValue inputValue)
@@ -30,10 +36,19 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag =="PickUp")
         {
             other.gameObject.SetActive(false);
-
             count += 1;
+            SetCountText();
         }
 
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups)
+        {
+            winText.text = "You Win!";
+        }
     }
 
 }
