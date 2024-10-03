900bf97564da5cf54726dbb03e47c754f626e505
aamirnaje <aamirnaje16@gmail.com>
2024-10-03 19:55:14 +0100

    4.2.2-completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,17 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private int count;
     private int numPickups = 5;
+    private Vector3 lastposition;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI winText;
-
+    public TextMeshProUGUI playerPositionText;
+    public TextMeshProUGUI playerVelocityText;
     void Start()
     {
         count = 0;
         winText.text = "";
         SetCountText();
-        
+        lastposition = transform.position;
     }
     void OnMove(InputValue inputValue)
     {
@@ -29,11 +31,17 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(moveValue.x, 0f, moveValue.y);
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+
+        //Player Position
+        playerPositionText.text = "Player Position: " + transform.position.ToString("0.00");
+        //player Velocity
+        playerVelocityText.text = "Player Velocity: " + (((transform.position - lastposition) / Time.deltaTime)).magnitude.ToString("0.00");
+        lastposition = transform.position;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag =="PickUp")
+        if (other.gameObject.tag == "PickUp")
         {
             other.gameObject.SetActive(false);
             count += 1;
@@ -45,7 +53,7 @@ public class PlayerController : MonoBehaviour
     private void SetCountText()
     {
         scoreText.text = "Score: " + count.ToString();
-        if(count >= numPickups)
+        if (count >= numPickups)
         {
             winText.text = "You Win!";
         }
